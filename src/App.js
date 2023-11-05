2a9a6c99970f0891078133c225d6a3ab2db175f7
Srawan Meesala <srawanviz@gmail.com>
2023-11-05 16:51:29 +0530

    commit-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card-1">
+        <div className="hello">Hello. I am</div>
+        <div className="name">SRAWAN MEESALA</div>
+        <div className="card-1-role">
+          <div className="card-1-role-text">
+            Software Development.<br /> Front-End Development.<br /> Web3.0 Development.
+          </div>
+        </div>
+      </div>
+      <div className="card-2">
+        <div className="card-2-head">Myself.</div>
+        <div className="card-2-content">
+          <div className="card-2-content-text">
+            Born in <span>Vizianagaram, AndhraPradesh, India.</span><br />
+            Bachelor of Technology from <span>Indian Institute Of Information Technology, Sri City.</span><br />
+            My mission is to bring <span>innovation</span> to life, <span>one line of code</span> at a time. <br />
+            I'm a firm believer in the endless possibilities that <span>technology</span> can unlock, and I'm here to make those possibilities<span> a reality.</span>
+          </div>
+        </div>
+      </div>
+      <div className="card-3">
+        <div className="card-2-head">Links.</div>
+        <div className="card-2-content">
+          <div className="card-2-content-text">
+            <a href="mailto: srawanviz@gmail.com">
+              E-Mail <span>srawanviz@gmail.com</span><br />
+            </a>
+            <a href="https://www.linkedin.com/in/srawan-meesala-35b3b3242/">
+              Linked in <span>srawan meesala</span><br />
+            </a>
+            <a href="https://github.com/srawan-meesala">
+              github <span>@srawan-meesala</span><br />
+            </a>
+            <a href="https://www.instagram.com/srawan_meesala/">
+              Instagram<span>@srawan_meesala</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="card-4">
+      <div className="card-2-head">Projects.</div>
+        
+      </div>
     </div>
   );
 }
